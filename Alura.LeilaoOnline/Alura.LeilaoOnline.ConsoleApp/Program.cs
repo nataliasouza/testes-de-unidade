a32caf62f0a7cf6eeff4f68a7286c0e6e4f0f858
Natália Souza <nataliasouza.ti@hotmail.com>
2021-11-03 12:16:02 -0300

    Estrutura de um teste automatizado

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,66 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        private static void Verifica(double esperado, double obtido)
+        {
+            var cor = Console.ForegroundColor;
+            if (esperado == obtido)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Teste OK");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Teste Falhou! Esperado:{esperado}, obtido: {obtido}");
+            }
+            Console.ForegroundColor = cor;
+        }
+        private static void LeilaoComMuitosLances()
+        {
+            //Arrange - cenário
+            var leilao = new Leilao("Van Gogh");
+            var max = new Interessada("Fulano", leilao);
+            var mallu = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(mallu, 700);
+            leilao.RecebeLance(max, 920);
+            leilao.RecebeLance(mallu, 1000);
+            leilao.RecebeLance(max, 980);
+
+            //ACT - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+            Verifica(valorEsperado, valorObtido);
+        }
+
+        private static void LeilaoComUmLance()
+        {
+            //Arrange - cenário
+            var leilao = new Leilao("Van Gogh");
+            var max = new Interessada("Fulano", leilao);
+      
+            leilao.RecebeLance(max, 700);
+
+            //ACT - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 700;
+            var valorObtido = leilao.Ganhador.Valor;
+            Verifica(valorEsperado, valorObtido);
+        }
+        static void Main(string[] args)
+        {
+            LeilaoComMuitosLances();
+            LeilaoComUmLance();
+        }
+    }
+}
