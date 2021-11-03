04b90409ae62051241dfbe4c4b1fdd9f50c4b6cb
Natália Souza <nataliasouza.ti@hotmail.com>
2021-11-03 17:39:28 -0300

    [Theory]

--- Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -4,75 +4,39 @@ using Xunit;
 namespace Alura.LeilaoOnline.Tests
 {
     public class LeilaoTestes
-    {
-        [Fact]
-        public void LeilaoComTresClintes()
-        {
-            var leilao = new Leilao("Van Gogh");
-            var teddy = new Interessada("Teddy", leilao);
-            var mike = new Interessada("Mike", leilao);
-            var nat = new Interessada("Nat", leilao);
-
-            leilao.RecebeLance(mike, 800);
-            leilao.RecebeLance(teddy, 950);
-            leilao.RecebeLance(nat, 975);
-            leilao.RecebeLance(teddy, 1100);
-
-            leilao.TerminaPregao();
-
-            var valorEsperado = 1100;
-            var valorObtido = leilao.Ganhador.Valor;
-            Assert.Equal(valorEsperado, valorObtido);
-        }
-        [Fact]
-        public void LeilaoComLancesOrdenadosPorValor()
-        {           
-            var leilao = new Leilao("Van Gogh");
-            var max = new Interessada("Max", leilao);
-            var mallu = new Interessada("Mallu", leilao);
-
-            leilao.RecebeLance(mallu, 700);
-            leilao.RecebeLance(max, 920);
-            leilao.RecebeLance(max, 980);
-            leilao.RecebeLance(mallu, 1000);       
-
-            leilao.TerminaPregao();
-
-            var valorEsperado = 1000;
-            var valorObtido = leilao.Ganhador.Valor;
-            Assert.Equal(valorEsperado, valorObtido);
-        }
-        [Fact]
-        public void LeilaoComMuitosLances()
+    {               
+        [Theory]
+        [InlineData(1250, new double[] { 800, 920, 1000, 1250})]
+        [InlineData(1000,new double[] { 800, 920, 1000, 980 })]
+        [InlineData(800, new double[] { 800 })]
+        public void LeilaoComMuitosLances(
+            double valorEsperado,
+            double[]ofertas)
         {
             var leilao = new Leilao("Van Gogh");
             var max = new Interessada("Max", leilao);
-            var mallu = new Interessada("Mallu", leilao);
-
-            leilao.RecebeLance(mallu, 700);
-            leilao.RecebeLance(max, 920);
-            leilao.RecebeLance(mallu, 1000);
-            leilao.RecebeLance(max, 980);
+           
+            foreach(var valor in ofertas)
+            {
+                leilao.RecebeLance(max, valor);
+            }
 
             leilao.TerminaPregao();
-
-            var valorEsperado = 1000;
+         
             var valorObtido = leilao.Ganhador.Valor;
             Assert.Equal(valorEsperado, valorObtido);
         }
-       
-        [Fact]
-        public void LeilaoComUmLance()
-        {
-            var leilao = new Leilao("Van Gogh");
-            var max = new Interessada("Max", leilao);
 
-            leilao.RecebeLance(max, 700);
+        [Fact]
+        public void LeilaoSemLances()
+        {
+            var leilao = new Leilao("Van Gogh");                
 
             leilao.TerminaPregao();
 
-            var valorEsperado = 700;
+            var valorEsperado = 0;
             var valorObtido = leilao.Ganhador.Valor;
+
             Assert.Equal(valorEsperado, valorObtido);
         }
     }
